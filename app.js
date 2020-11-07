6a0dc9c3a553aa8d19b08f0b16e05177be3b36e4
Pawel Zimin <paweu14@gmail.com>
2020-11-07 15:30:53 +0100

    Repaired non-fluent image change in modal

--- app.js
+++ app.js
@@ -4,14 +4,24 @@ const modalCaption = document.querySelector('.modal__img-caption');
 const galleryImgs = document.querySelectorAll('.img-container img');
 
 const modalOpener = (e) => {
+   modalImg.src = e.target.getAttribute('data-original');
    modalCaption.textContent = e.target.alt;
-   modalImg.src = e.target.getAttribute('data-original')
    modal.classList.add('active');
 }
 
 const modalCloser = (e) => {
    if(e.target.classList.contains('modal')){
       modal.classList.remove('active');
+      galleryImgs.forEach(img => {
+         img.removeEventListener('click', modalOpener);
+      });
+
+      setTimeout(() => {
+         modalImg.src = '';
+         galleryImgs.forEach(img => {
+            img.addEventListener('click', modalOpener);
+         });
+      }, 500);
    }
 }
 
